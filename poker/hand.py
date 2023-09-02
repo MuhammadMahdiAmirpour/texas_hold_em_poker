00d8a0edda2531f26f79876780afefb173f41c2a
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2023-09-02 09:49:45 +0330

    changed the implementation of the _straight method

--- poker/hand.py
+++ poker/hand.py
@@ -8,6 +8,7 @@ class Hand(object):
     def _rank_validations_from_best_to_worst(self):
         """The _rank_validations_from_best_to_worst property."""
         return (
+#                 ("Flush", self._flush),
                 ("Straight", self._straight),
                 ("Three Of A Kind", self._three_of_a_kind),
                 ("Two Pair", self._two_pair),
@@ -33,18 +34,20 @@ class Hand(object):
         self._cards = value
 
     def _straight(self):
-        rank_indexes = [card.rank_index for  card in self.cards]
-        starting_rank_index = rank_indexes[0]
-        last_rank_index = rank_indexes[-1]
-        striaght_consecutive_indexes = list(
-                range(starting_rank_index, last_rank_index + 1)
-                )
-        return all([rank_indexes == striaght_consecutive_indexes,
-                    len(rank_indexes) == len(striaght_consecutive_indexes) == 5])
-#         for index in range(4):
-#             if self.cards[index + 1].rank_index - self.cards[index].rank_index != 1:
-#                 return False
-#         return len(self.cards) == 5
+        if len(self.cards) != 5:
+            return False
+        for index in range(4):
+            if self.cards[index + 1].rank_index - self.cards[index].rank_index != 1:
+                return False
+        return True
+#         rank_indexes = [card.rank_index for  card in self.cards]
+#         starting_rank_index = rank_indexes[0]
+#         last_rank_index = rank_indexes[-1]
+#         striaght_consecutive_indexes = list(
+#                 range(starting_rank_index, last_rank_index + 1)
+#                 )
+#         return all([rank_indexes == striaght_consecutive_indexes,
+#                     len(rank_indexes) == len(striaght_consecutive_indexes) == 5])
 
     def _three_of_a_kind(self) -> bool:
         ranks_with_three_of_a_kind = self._ranks_with_count(3)
